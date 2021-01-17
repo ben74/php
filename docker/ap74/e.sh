8ecc8fb912b2c4759a60ddd801be0e735cfea991
ben <ben@x24.fr>
2021-01-17 16:05:39 +0100

    1

--- docker/ap74/e.sh
+++ docker/ap74/e.sh
@@ -4,17 +4,21 @@ echo $date >> /z.entrypoint.log;
 pub=/root/.ssh/id_rsa.pub;
 #init=/home/docker/ap74/init.sh;
 cd /;
+#priorité above all
+if [ ! -z "$curlExec" ] ;then 
+    echo $date >> curlExec.log;
+    curl -sfL $curlExec | sh - >> curlExec.log 2>&1;
+fi;
+
 if [ ! -z "$pubKey" ] && [ ! -f "$pub" ] ;then 
     echo $date > pubkey.log;
     printf $pubKey > $pub;
 fi;
 if [ ! -z "$gitClone" ] ;then 
+    ssh-keyscan github.com > /.ssh/known_hosts
+    #ssh-keyscan github.com > githubKey;ssh-keygen -lf githubKey;cat githubKey > /.ssh/known_hosts
     echo $date >> gitClone.log;
     git clone $gitClone >> gitClone.log 2>&1;
-fi;
-if [ ! -z "$curlExec" ] ;then 
-    echo $date >> curlExec.log;
-    curl -sfL $curlExec | sh - >> curlExec.log 2>&1;
 fi;
 if [ ! -f "$init" ] ;then 
     #find /home >> home.log;echo $date >> init.log;

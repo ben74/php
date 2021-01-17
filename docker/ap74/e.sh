36d6678b76057fdd56a170d4f779767ec84a244d
ben <ben@x24.fr>
2021-01-17 15:33:26 +0100

    composeYaml

--- docker/ap74/e.sh
+++ docker/ap74/e.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 date=`date +%y%m%d%H%M`;
 echo $date >> /z.entrypoint.log;
-init=/home/docker/ap74/init.sh;if [ -f "$init" ] ;then 
+#init=/home/docker/ap74/init.sh;
+if [ -f "$init" ] ;then 
     find /home >> home.log;echo $date >> init.log;bash $init >> init.log 2>&1 &
 fi;
 #init=/root/.bashrc;if [ -f "$init" ] ;then cp / /root/.bashrc;fi;
